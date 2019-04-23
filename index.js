7ffb1b4b055b8a4cff9c415c204796b5c119a7c3
Keenan Cahill <thatkeenancahill@gmail.com>
2019-04-23 22:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var greeting = "hello. everyone";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "hello" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}.`;
+
